2360bd71f9bddeb70243b422df6a1e99818f2cef
irfan meraj <postswapnetwork@gmail.com>
2025-07-09 20:00:30 +0500

    dashboard changes

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
       <div>
         <h2 className="text-white mb-4">Dashboard</h2>
         <div className="row">
-          <div className="col-md-4 col-lg-3 col-sm-4 col-12">
+          <div className="col-md-4 col-lg-4 col-sm-4 col-12">
             <div className=" card bg-primary text-white mb-4">
               <div className="card-body">
                 <h5 className="card-title ">Total Users</h5>
@@ -14,7 +14,7 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-           <div className="col-md-4 col-lg-3 col-sm-4 col-12">
+          <div className="col-md-4 col-lg-4 col-sm-4 col-12">
             <div className=" card bg-success text-white mb-4">
               <div className="card-body">
                 <h5 className="card-title ">Total Posts</h5>
@@ -22,7 +22,7 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-           <div className="col-md-4 col-lg-3 col-sm-4 col-12">
+          <div className="col-md-4 col-lg-4 col-sm-4 col-12">
             <div className=" card bg-warning text-white mb-4">
               <div className="card-body">
                 <h5 className="card-title ">Total Comments</h5>
